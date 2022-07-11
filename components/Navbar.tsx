e296dd24e36c69bb902df63a218fd7f3fa31aa9e
DivineDemon <supame123@gmail.com>
2022-07-11 15:52:20 +0400

    :hammer:

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
@@ -10,9 +10,15 @@ import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import Logo from "../utils/tiktik-logo.png";
 import { createOrGetUser } from "./../utils";
 import useAuthStore from "./../store/authStore";
+import { IUser } from '../types';
 
 const Navbar = () => {
+  const [user, setUser] = useState<IUser | null>();
   const { userProfile, addUser, removeUser } = useAuthStore();
+
+  useEffect(() => {
+    setUser(userProfile);
+  }, [userProfile]);
 
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 p-2 px-4">
@@ -36,14 +42,14 @@ const Navbar = () => {
                 <span className="hidden md:block">Upload</span>
               </button>
             </Link>
-            {userProfile.image && (
+            {user?.image && (
               <Link href="">
                 <>
                   <Image
                     width={40}
                     height={40}
                     className="rounded-full"
-                    src={userProfile.image}
+                    src={user?.image}
                     alt="profile"
                   />
                 </>
